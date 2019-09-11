4e0161a090f3bc7b04942a244c85c9b0599330c2
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-09-11 15:28:14 -0700

    [updated] load_word to deleted spaces

--- Spaceman.py
+++ Spaceman.py
@@ -1,11 +1,17 @@
 import random
 
-num_guess = 8
-
 def load_word():
     f = open('/usr/share/dict/words', 'r')
     words_list = f.readlines()
     f.close()
+
+    new_list = []
+
+    for word in words_list:
+        new_word = word.strip()
+        new_list.append(new_word)
+
+    words_list = new_list
 
     #words_list = words_list[0].split(' ') #comment this line out if you use a words.txt file with each word on a new line
     secret_word = random.choice(words_list)
@@ -27,12 +33,13 @@ def is_guess_in_word(guess, secret_word):
     return guess in secret_word
 
 def spaceman(secret_word):
+    print("––––––––––––––––––––––––––––––––––––––––––––––––––––––––––")
     print("Thank you for choosing to help our Spaceman!")
     print("Lets help the Spaceman find his way home.")
     print("You are allowed to guess 1 letter per round.")
     print("There are " + str(len(secret_word)) + " letters." )
     print("Hurry up and save the Spaceman!")
-
+    print("––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\n")
     running = True
     word_guessed = []
     num_guess = 0
@@ -42,7 +49,7 @@ def spaceman(secret_word):
         guess = ""
 
         while True:
-            guess = input("Please input a letter: ")
+            guess = input("\nPlease input a letter: ")
             if len(guess) == 1 and guess.isalpha():
                 break
             else:
@@ -50,10 +57,12 @@ def spaceman(secret_word):
 #TODO: Check if the guessed letter is in the secret or not and give the player feedback
 
         if is_guess_in_word(guess, secret_word_chars):
-            print("Nice! You are a step closer to saving our Spaceman!")
+            print("\n–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––")
+            print("\nNice! You are a step closer to saving our Spaceman!\n")
             word_guessed.append(guess)
         else:
-            print("Good try! Try again")
+            print("\n–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––")
+            print("\nGood try! Try again\n")
             num_guess += 1
 
 #TODO: show the guessed word so far
@@ -63,8 +72,10 @@ def spaceman(secret_word):
 #TODO: check if the game has been won or lost
 
         if is_word_guessed(secret_word_chars, word_guessed):
-            print("You have guessed the sercet word!")
-            print("Thank you for helping our Spaceman return home!")
+            print("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––")
+            print("\nYou have guessed the sercet word!\n")
+            print("The secret word was: " + secret_word)
+            print("\nThank you for helping our Spaceman return home!")
             print("\nYour skills are valuable, would you like to help another Spaceman?\n")
             function_code = input("Accept your mission? (Y/N): ")
             if function_code.upper() == "Y":
@@ -73,10 +84,11 @@ def spaceman(secret_word):
                 break
 
         if num_guess == 7:
-            print("Oh no! You weren't able to save our Spaceman!")
-            print("Thank you for trying, I know the conditions were sereve.")
+            print("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––")
+            print("\nOh no! You weren't able to save our Spaceman!")
+            print("Thank you for trying, I know the conditions were seve.\n")
             print("The secret word was: " + secret_word)
-            print("\nYour skills are valuable, would you like to help another Spaceman?\n")
+            print("Your skills are valuable, would you like to help another Spaceman?\n")
             function_code = input("Accept your mission? (Y/N): ")
             if function_code.upper() == "Y":
                 spaceman(load_word)
